926868ff400b94de70c060f6846ab85c464508ee
Lazar Petrović <lazar.petrovic2310@gmail.com>
2023-01-11 13:06:52 +0100

    timers no work :(

--- js/algorithms.js
+++ js/algorithms.js
@@ -3,20 +3,38 @@ import { sleep } from './sleep.js';
 import { update_chart, update_chart_color } from './bar_chart.js';
 
 export async function sort_array(chart) {
+    const timer_HTML = document.getElementById('elapsed_time');
+    let is_running = true;
+
     switch (animation.algorithm_type) {
         case 'selection_sort':
-            await selection_sort(chart);
+            selection_sort(chart).then(() => { is_running = false; });
             break;
         case 'bubble_sort':
-            await bubble_sort(chart);
+            bubble_sort(chart).then(() => { is_running = false; });
             break;
         case 'merge_sort':
-            await merge_sort(chart);
+            merge_sort(chart).then(() => { is_running = false; });
             break;
         default:
             alert("Uh oh something broke...");
-            break;
+            return;
     }
+
+    const timer = async function() {
+        const time_at_start = Date.now() - animation.time_at_pause;
+        let time_elapsed = 0;
+        
+        while (is_running) {
+            time_elapsed = Date.now() - time_at_start;
+            timer_HTML.innerHTML = `${time_elapsed}ms (${time_elapsed / 1000}s)`;
+            await sleep (150);
+        }
+
+        animation.time_at_pause = time_elapsed;
+    };
+
+    timer();
 }
 
 // Swap two numbers in the array.
@@ -29,9 +47,6 @@ function swap(x, y) {
 // Selection sort.
 async function selection_sort(chart) {
     const arr_size = animation.array_size;
-    const timer_HTML = document.getElementById('elapsed_time');
-    
-    let time_at_start = Date.now() - animation.time_at_pause;
 
     for (let i = animation.state; i < arr_size - 1; i++) {
         animation.state = i;
@@ -47,36 +62,25 @@ async function selection_sort(chart) {
 
         swap(i, min_i);
         
-        const time_elapsed = Date.now() - time_at_start;
-        timer_HTML.innerHTML = `${time_elapsed}ms (${time_elapsed / 1000}s)`;
-        
-        time_at_start += await sleep(animation.sorting_speed);
+        await sleep(animation.sorting_speed);
 
         if (!animation.is_playing)
             break;
-        else
-            animation.time_at_pause = time_elapsed;
     }
 
     update_chart(chart);
     end_animation();
 }
 
-// Bubble sort.
+// Bubble sort. (UNOPTIMIZED)
 async function bubble_sort(chart) {
     const arr_size = animation.array_size;
-    const timer_HTML = document.getElementById('elapsed_time');
 
-    let time_at_start = Date.now() - animation.time_at_pause;
-
-    for (let i = animation.state; i < arr_size - 1; i++)
-    {
-        for (let j = 0; j < arr_size - i - 1; j++)
-        {
+    for (let i = animation.state; i < arr_size - 1; i++) {
+        for (let j = 0; j < arr_size - i - 1; j++) {
             animation.state = j;
             
-            if (animation.array[j] > animation.array[j + 1])
-            {
+            if (animation.array[j] > animation.array[j + 1]) {
                 update_chart_color(chart, j + 1);
                 swap(j, j + 1);
             }
@@ -84,19 +88,12 @@ async function bubble_sort(chart) {
                 update_chart_color(chart);
             }
 
-            const time_elapsed = Date.now() - time_at_start;
-            timer_HTML.innerHTML = `${time_elapsed}ms (${time_elapsed / 1000}s)`;
-            
-            time_at_start += await sleep(animation.sorting_speed);
+            await sleep(animation.sorting_speed / arr_size - 1);
 
-            if (!animation.is_playing)
-            {
+            if (!animation.is_playing) {
                 animation.state = i;
                 i = arr_size;
                 break;
-            }
-            else {
-                animation.time_at_pause = time_elapsed;
             }
         }
     }
@@ -106,14 +103,21 @@ async function bubble_sort(chart) {
 }
 
 // Merge sort.
-async function merge_sort(chart, left_i = 0, right_i = animation.array_size - 1) {
+async function merge_sort(chart) {
+    await merge_sort_rec(chart);
+
+    update_chart(chart);
+    end_animation();
+}
+
+async function merge_sort_rec(chart, left_i = 0, right_i = animation.array_size - 1) {
     if (left_i >= right_i)
         return;
 
     const middle_i = left_i + parseInt((right_i - left_i) / 2);
 
-    await merge_sort(chart, left_i, middle_i);
-    await merge_sort(chart, middle_i + 1, right_i);
+    await merge_sort_rec(chart, left_i, middle_i);
+    await merge_sort_rec(chart, middle_i + 1, right_i);
     await merge(chart, left_i, middle_i, right_i);
 }
 
@@ -161,6 +165,7 @@ async function merge(chart, left_i, middle_i, right_i) {
         k++;
     }
 
-    update_chart(chart);
+    animation.state = left_i;
+    update_chart_color(chart, right_i);
     await sleep (animation.sorting_speed);
 }
